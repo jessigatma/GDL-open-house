6174efa5048285650f94d5fb734a7fdfcc24ce8e
jessigatma <jessgat@hotmail.com>
2019-05-30 18:43:32 -0500

    merging responsive and modals

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,30 @@
-<<<<<<< HEAD
-function showModal(){
+function showModal() {
     document.getElementById("modal").style.display = 'block';
-    
-    }
-    document.getElementById("becomeDriver").addEventListener('click',showModal);
-   
 
-    function showModalR(){
-        document.getElementById("modalRegister").style.display='block';
-    }
-    document.getElementById("userRegister").addEventListener('click',showModalR);
+}
+document.getElementById("becomeDriver").addEventListener('click', showModal);
+document.getElementById("becomeDriver1").addEventListener('click', showModal);
 
-    
-    function hiddenModal(){
-        document.getElementById("modal").style.display='none';
-        document.getElementById("modalRegister").style.display='none';
-    }
-    document.querySelector(".close").addEventListener('click',hiddenModal);
-    
+function showModalR() {
+    document.getElementById("modalRegister").style.display = 'block';
+}
+document.getElementById("userRegister").addEventListener('click', showModalR);
 
-   
-=======
 
-function showMenu(){
+function hiddenModal() {
+    document.getElementById("modal").style.display = 'none';
+}
+document.getElementById("close2").addEventListener('click', hiddenModal);
+document.getElementById("close1").addEventListener('click', hiddenModal);
+
+
+
+function showMenu() {
     document.getElementById("id01").style.display = 'block';
-    }
-    document.getElementById("menuBtn").addEventListener('click',showMenu);
+}
+document.getElementById("menuBtn").addEventListener('click', showMenu);
 
-function closeMenu(){
-        document.getElementById("id01").style.display = 'none';
-        }
-        document.getElementById("btnCloseMenu").addEventListener('click',closeMenu);
->>>>>>> 9e91533ded1fc8af07127a3fed37aa4650a7b26e
+function closeMenu() {
+    document.getElementById("id01").style.display = 'none';
+}
+document.getElementById("btnCloseMenu").addEventListener('click', closeMenu);
