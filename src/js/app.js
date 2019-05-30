7a37087b68fe7e34bc6ddd1a93a4de1ca86bc4e2
jessigatma <jessgat@hotmail.com>
2019-05-30 15:05:02 -0500

    starting the modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
 // La funcionalidad de tu proyecto
+window.onclick = (event) => {
+    let modal = document.getElementById('modal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
