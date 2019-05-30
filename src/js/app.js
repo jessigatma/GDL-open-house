9e91533ded1fc8af07127a3fed37aa4650a7b26e
SofiaRuizHinojosa27 <srh_19@hotmail.com>
2019-05-30 16:39:57 -0500

    the menu bar in mobile version, already displays your options

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
-// La funcionalidad de tu proyecto
+
+function showMenu(){
+    document.getElementById("id01").style.display = 'block';
+    }
+    document.getElementById("menuBtn").addEventListener('click',showMenu);
+
+function closeMenu(){
+        document.getElementById("id01").style.display = 'none';
+        }
+        document.getElementById("btnCloseMenu").addEventListener('click',closeMenu);
