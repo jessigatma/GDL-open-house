7f94328330a36aaae4b567cdea494807e3a5faee
jessigatma <jessgat@hotmail.com>
2019-05-30 18:04:35 -0500

    modals completed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 // La funcionalidad de tu proyecto
+function showModal(){
+    document.getElementById("modal").style.display = 'block';
+    
+    }
+    document.getElementById("becomeDriver").addEventListener('click',showModal);
+   
+
+    function showModalR(){
+        document.getElementById("modalRegister").style.display='block';
+    }
+    document.getElementById("userRegister").addEventListener('click',showModalR);
+
+    
+    function hiddenModal(){
+        document.getElementById("modal").style.display='none';
+        document.getElementById("modalRegister").style.display='none';
+    }
+    document.querySelector(".close").addEventListener('click',hiddenModal);
